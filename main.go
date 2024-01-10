ed91de2337e87202d7618057a00df9e2fef542eb
vay4ie <vay4ie@gmail.com>
2024-01-10 16:12:48 +0200

    Played w/ program & added different function support

--- main.go
+++ main.go
@@ -3,26 +3,33 @@ package main
 import (
 	"image"
 	"log"
+	"math"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-func f(x float64) float64  { return x*x + 5*x - 3 }
-func df(x float64) float64 { return 2*x + 5 }
+//FUNCTIONS
+
+//func f(x float64) float64 { return x*x + 3*x - 5 }
+
+func f(x float64) float64 { return math.Cos(x) }
+
+// func f(x float64) float64 { return math.Sin(x) }
+//func f(x float64) float64 { return 0.1*(x*x) - math.Sqrt(97)*x + 10 }
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Gradient descent")
+	ebiten.SetWindowTitle("Meh")
 
 	img := make(chan *image.RGBA, 1)
 	go func() {
-		p := Plot(-5, 0, 0.1, f)
-		x := 0.0
+		p := Plot(-10, 10, 0.1, f)
+		x := 0.25
 		img <- p(x)
-		for i := 0; i < 50; i++ {
-			time.Sleep(30 * time.Millisecond)
-			x -= df(x) * 0.1
+		for i := 0; d(f, x) != 0; i++ {
+			time.Sleep(500 * time.Millisecond)
+			x -= d(f, x) * 0.9
 			img <- p(x)
 		}
 	}()
@@ -30,4 +37,19 @@ func main() {
 	if err := ebiten.RunGame(&App{Img: img}); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Isolated one-dimensional gradient descent function (not used)
+func GradientDescent(f func(float64) float64) float64 {
+	x := 0.25
+	for i := 0; d(f, x) != 0; i++ {
+		x -= d(f, x) * 0.9
+	}
+	return x
+}
+
+// Derivative
+func d(f func(float64) float64, x float64) float64 {
+	h := 0.0001
+	return (f(x+h) - f(x)) / h
 }
