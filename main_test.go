7f72a9ff907bcad42511b8c038e904e11be05afd
Littleunidragon <a.voropajeva3@gmail.com>
2024-01-10 19:40:42 +0200

    i am commited now

--- main_test.go
+++ main_test.go
@@ -17,9 +17,16 @@ func TestDerivative(t *testing.T) {
 		{name: "simple square", f: func(x float64) float64 { return x * x }, x: 2.0, want: 4.0},
 		{name: "not so simple square", f: func(x float64) float64 { return x*x + 5*x - 3 }, x: 1.0, want: 7.0},
 		{name: "hyperbola", f: func(x float64) float64 { return 1 / x }, x: 2.0, want: -0.25},
-		//{name: "complex sin", f: func(x float64) float64 { return math.Sin(x*x + 5*x - 3) }, x: 1.0, want: 7.0},
-		//{name: "complex cos", f: func(x float64) float64 { return math.Cos(x*x + 5*x - 3) }, x: 1.0, want: -1.0},
-		// for last two precision is < 0.1 ha
+		{name: "diff.135 lpp. N18 a)", f: func(x float64) float64 { return 2*x*x - 3*x }, x: 3, want: 9},
+		{name: "diff.135 lpp. N18 b)", f: func(x float64) float64 { return x*x - x + 2 }, x: 0, want: -1},
+		{name: "diff.135 lpp. N18 c)", f: func(x float64) float64 { return 4 - 5*x - x*x }, x: 1, want: -7},
+		{"diff.135 lpp. N18 d)", func(x float64) float64 { return -3 / x }, 3, 0.3333333}, //1/3
+		{"diff.135 lpp. N18 e)", func(x float64) float64 { return 4 / x * x }, -2, 0},
+		{"diff.135 lpp. N18 f)", func(x float64) float64 { return 4/x + 1 }, -2, -1},
+		{"diff.135 lpp. N18 g)", func(x float64) float64 { return 6/2 - x }, -1, -1},
+		{"diff.135 lpp. N18 h)", func(x float64) float64 { return 1 / math.Sqrt(x) }, 4, -0.062499},
+		{"diff.135 lpp. N18 i)", func(x float64) float64 { return 2 * math.Sqrt(1-x) }, 0, -1},
+		{"diff.135 lpp. N18 j)", func(x float64) float64 { return math.Sqrt(1 + 2*x) }, 4, 0.3333333},
 	} {
 		t.Run("", func(t *testing.T) {
 			d := derivative(tc.f)
