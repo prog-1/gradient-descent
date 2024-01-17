da9d95ff28726a6b4ebf2d64e3893186a5dcd3ef
vay4ie <vay4ie@gmail.com>
2024-01-17 20:11:42 +0200

    Made linear regression w/o drawing
    Somehow overcame the core, but couldn't understand the drawing with plot

---
+++ Gradient_Descent/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"image"
+	"log"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func f(x float64) float64  { return x*x + 5*x - 3 }
+func df(x float64) float64 { return 2*x + 5 }
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Gradient descent")
+
+	img := make(chan *image.RGBA, 1)
+	go func() {
+		p := Plot(-5, 0, 0.1, f)
+		x := 0.0
+		img <- p(x)
+		for i := 0; i < 50; i++ {
+			time.Sleep(30 * time.Millisecond)
+			x -= df(x) * 0.1
+			img <- p(x)
+		}
+	}()
+
+	if err := ebiten.RunGame(&App{Img: img}); err != nil {
+		log.Fatal(err)
+	}
+}
